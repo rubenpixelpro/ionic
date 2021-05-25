992924c8c823be811ddb9e9a0dc3d998e1fe3e58
Ruben <RUB@hotmail.com>
2021-05-25 18:07:52 +0200

    Video 2.10

---
+++ src/app/types.ts
@@ -0,0 +1,13 @@
+export interface Character {
+    Species: string
+    Age: string
+    Planet: string
+    Profession: string
+    Status: string
+    FirstAppearance: string
+    PicUrl: string
+    Relatives: string
+    VoicedBy: string
+    Name: string
+  }
+  
